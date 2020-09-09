0d38103a38f041aa0b3030d347d1a6d31eaf5316
ajitalhatimi <ajit77shinde@gmail.com>
2020-09-10 03:34:28 +0530

    navbar and login page with validation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ValidatedLoginForm from './Components/ValidatedLoginForm';
+import NavBarComponent from './Components/NavBarComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NavBarComponent />
+      {/* <ValidatedLoginForm/> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
